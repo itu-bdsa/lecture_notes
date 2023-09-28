2af1f90695ff4b4c76a053a33401ffa9efe97ff7
HelgeCPH <ropf@itu.dk>
2023-09-28 14:25:58 +0200

    Automatic release of teaching material.

---
+++ sessions/session_05/Chirp.Razor/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
